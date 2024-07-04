c2d4b03aa6c4355e44c8545d16bf88f7aac78fc0
Mustapha tahiru <tahirumustapha13@gmail.com>
2024-07-04 00:46:35 +0000

    created register and login api

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,93 +1,94 @@
-import React from "react";
-import { FaAsterisk } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+// import { FaAsterisk } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Input, Button } from "antd";
 import axios from "axios";
 import toast from "react-hot-toast";
 const Register = () => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const onFinish = async (values) => {
     try {
-      const response = axios.post("/api/user/register", values);
+      setLoading(true);
+      const response = await axios.post("api/user/register", values);
+
       if (response.data.success) {
         toast.success(response.data.message);
+        form.resetFields();
+        navigate("/login");
       } else {
         toast.error(response.data.message);
+        setLoading(false);
       }
     } catch (error) {
       toast.error("Error registering user!");
+      setLoading(false);
     }
   };
   return (
     <div className=" bg-[#053B50] w-full h-full   authentication">
-      <div className=" bg-white  relative  shadow-md border-gray-300 border-[1px] rounded-md  w-[30%] px-3 py-5 authentication-form card">
-        <form onSubmit={onFinish} className=" w-full h-full mt-7">
-          <div className="  bg-[#F86F03]  absolute top-3 left-[-20px]   px-3 py-1 rounded-bl-xl">
-            <h1 className=" text-2xl ml-4     inline-block   text-white card-title capitalize">
-              Nice to meet you
-            </h1>
-          </div>
+      <div className=" bg-white    shadow-md border-gray-300 border-[1px] rounded-md  w-[30%] px-3 py-5 authentication-form card">
+        <h1 className=" ml-[-40px] bg-[#F86F03] px-3 py-1 rounded-bl-xl text-2xl     inline-block   text-white card-title capitalize">
+          Nice to meet you
+        </h1>
 
-          <div className=" flex flex-col">
-            <label
-              htmlFor="name"
-              className=" flex items-center gap-1 text-gray-400 mt-2 mb-1"
+        <Form
+          layout="vertical"
+          onFinish={onFinish}
+          className=" w-full h-full mt-7"
+        >
+          <Form.Item rules={[{ required: true }]} label="Name" name="name">
+            <Input placeholder="Name" />
+          </Form.Item>
+          <Form.Item rules={[{ required: true }]} label="Email" name="email">
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            rules={[{ required: true }]}
+            label="Password"
+            name="password"
+          >
+            <Input type="password" placeholder="Password" />
+          </Form.Item>
+          {loading ? (
+            <Button className=" primary-btn mt-3  bg-[#053B50] w-full text-white">
+              <svg
+                aria-hidden="true"
+                role="status"
+                className="inline w-4 h-4 me-3 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                />
+              </svg>
+              Registering user...
+            </Button>
+          ) : (
+            <Button
+              htmlType="submit"
+              className=" primary-btn mt-3  bg-[#053B50] w-full text-white"
             >
-              <span className=" text-[0.50rem] text-red-500">
-                <FaAsterisk />
-              </span>
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="  focus:outline-none focus:border-none border-gray-300 rounded-sm"
-            />
-          </div>
-          <div className=" flex flex-col">
-            <label
-              htmlFor="email"
-              className=" flex items-center gap-1 text-gray-400 mt-2 mb-1"
-            >
-              <span className=" text-[0.50rem] text-red-500">
-                <FaAsterisk />
-              </span>
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className=" outline-none focus:outline-none focus:border-none border-gray-300 rounded-sm"
-            />
-          </div>
-          <div className="  flex flex-col">
-            <label
-              htmlFor="password"
-              className=" flex items-center gap-1 text-gray-400 mt-2 mb-1"
-            >
-              <span className=" text-[0.50rem] text-red-500">
-                <FaAsterisk />
-              </span>
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className=" outline-none focus:outline-none focus:border-none border-gray-300 rounded-sm"
-            />
-          </div>
-          <button type="submit" className=" mt-3 py-2 bg-[#053B50] w-full text-white">
-            Register
-          </button>
+              Register
+            </Button>
+          )}
+
           <div className=" mt-2">
             <span>Already have an account?</span>
-            <Link to="/login" className=" text-blue-700">
+            <Link to="/login" className=" text-blue-700 hover:underline">
               {" "}
               Login
             </Link>
           </div>
-        </form>
+        </Form>
       </div>
     </div>
   );
